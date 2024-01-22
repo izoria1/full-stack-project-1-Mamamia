4347593ba3b1e98bdba26e78eb61925104d2dab9
Giorgi <techshark4@gmail.com>
2024-01-23 02:22:47 +0400

    added book view. updated urls

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from restaurant.views import BookingViewSet
+
+# Create a router and register the BookingViewSet with it.
+router = DefaultRouter()
+router.register(r'', BookingViewSet)
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('restaurant/', include('restaurant.urls')),
-   path('restaurant/menu/',include('restaurant.urls')),
+    path('admin/', admin.site.urls),
+    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),  # Directly include router URLs here
 ]
